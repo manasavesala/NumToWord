268add5d4e6254960cd295c01ebcb4bb2047076a
Manasa Vesala <vesalamanasa@gmail.com>
2018-11-17 21:45:15 -0800

    added words for < 1000

--- NumToWord/Models/NumToWord.cs
+++ NumToWord/Models/NumToWord.cs
@@ -35,7 +35,8 @@ namespace NumberToWord.Models
           {70, "seventy"},
           {80, "eighty"},
           {90, "ninety"},
-          {100, "hundred"}
+          {100, "hundred"},
+          {1000, "thousand"},
 
          };
 
@@ -59,7 +60,18 @@ namespace NumberToWord.Models
       number = numberBackup;
       
       Console.WriteLine($"number = {number}");
-      if(number > 100 && number < 1000)
+      if(number > 1000 && number < 9999)
+      { 
+        int thousandaPlace = array[3];
+        int hundredsPlace = array[2];  
+        int tensPlace = array[1] * 10;
+        int onesPlace = array[0];
+        string four = dic[thousandaPlace]+" "+dic[1000];
+        string three = dic[hundredsPlace]+" "+ dic[100];
+        string two = dic[tensPlace];
+        string one = dic[onesPlace];
+        word = four+" "+three +" "+two + " " + one ; 
+      }else if(number > 100 && number < 1000)
       { 
         int hundredsPlace = array[2];  
         int tensPlace = array[1] * 10;
